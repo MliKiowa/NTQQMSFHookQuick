def18f939d2fac12554dc30b6bf313623334ae65
手瓜一十雪 <nanaeonn@outlook.com>
2024-10-09 11:17:48 +0800

    fix

--- hook-msf.js
+++ hook-msf.js
@@ -20,28 +20,34 @@ async function main() {
     Interceptor.attach(hook_recv, {
         onEnter(args) {
             console.log('----------------------');
+
             let cmd = Memory.readPointer(args[1]).add(32);
             let cmd_type = new Uint8Array(cmd.readByteArray(1))[0] & 0x1;
             let seq = Memory.readPointer(args[1]).add(24);
-            let uin = Memory.readPointer(args[1]).add(1);// remove error parse
+            let uin = Memory.readPointer(args[1]);
+            let uin_type = new Uint8Array(uin.readByteArray(1))[0] & 0x1;
             let bufferPtr =  Memory.readPointer(Memory.readPointer(args[1]).add(56));
             let buffer_start = Memory.readPointer(bufferPtr);
             let buffer_end = Memory.readPointer(bufferPtr.add(8));
             let buffer_len = buffer_end - buffer_start;
-            let uin_str = Memory.readUtf8String(uin);
 
-            if(isNaN(parseInt(uin_str))) uin_str = 'unknow';
+            let _cmd = cmd_type == 0 ? Memory.readUtf8String(cmd.add(1)) : Memory.readUtf8String(Memory.readPointer(cmd.add(16)));
+            let _uin = uin_type == 0 ? Memory.readUtf8String(uin.add(1)) : Memory.readUtf8String(Memory.readPointer(uin.add(16)));
+            let _seq = Memory.readU32(seq);
+            let buffer_hex = bytesToHex(buffer_start.readByteArray(buffer_len));
 
             console.log('type: recv');
-            console.log("seq:", Memory.readU32(seq));
-            let _cmd = cmd_type == 0 ? Memory.readUtf8String(cmd.add(1)) : Memory.readUtf8String(Memory.readPointer(cmd.add(16)));
+            console.log("seq:",_seq);
             console.log("cmd:", _cmd);
-            console.log("uin:", uin_str);
+            console.log("cmd_type:", cmd_type);
+            console.log("uin:", _uin);
+            console.log("uin_type:", uin_type);
             console.log("buffer_start:", buffer_start);
             console.log("buffer_ptr:", bufferPtr);
             console.log("buffer_end:", buffer_end);
             console.log("buffer_len:", buffer_len);
-            console.log("hex buffer_start", bytesToHex(buffer_start.readByteArray(buffer_len)));
+            console.log("hex buffer_start", buffer_hex);
+
             console.log('----------------------');
         },
         onLeave(retval) {
@@ -53,28 +59,32 @@ async function main() {
     Interceptor.attach(hook_send, {
         onEnter(args) {
             console.log('----------------------');
+            
             let seq = Memory.readPointer(args[1]).add(64);
-            let uin = Memory.readPointer(args[1]).add(32).add(1);// remove error parse
-            let uin_str = Memory.readUtf8String(uin);
+            let uin = Memory.readPointer(args[1]).add(32);
             let cmd = Memory.readPointer(Memory.readPointer(args[1]));
             let cmd_type = new Uint8Array(cmd.readByteArray(1))[0] & 0x1;
             let bufferPtr = Memory.readPointer(Memory.readPointer(Memory.readPointer(args[1])).add(32));
             let buffer_start = Memory.readPointer(bufferPtr);
             let buffer_end = Memory.readPointer(bufferPtr.add(8));
             let buffer_len = buffer_end - buffer_start;
-            if (isNaN(parseInt(uin_str))) uin_str = 'unknow';
+            
+            let _cmd = cmd_type == 0 ? Memory.readUtf8String(cmd.add(1)) : Memory.readUtf8String(Memory.readPointer(cmd.add(16)));
+            let _uin = uin_type == 0 ? Memory.readUtf8String(uin.add(1)) : Memory.readUtf8String(Memory.readPointer(uin.add(16)));
+            let _seq = Memory.readU32(seq);
+            let buffer_hex = bytesToHex(buffer_start.readByteArray(buffer_len));
 
             console.log('type: send');
-            console.log("seq:", Memory.readU32(seq));
-            console.log("uin:", uin_str);
-            let _cmd = cmd_type == 0 ? Memory.readUtf8String(cmd.add(1)) : Memory.readUtf8String(Memory.readPointer(cmd.add(16)));
+            console.log("seq:", _seq);
+            console.log("uin:", _uin);
             console.log("cmd:", _cmd);
             console.log("cmd_type:", cmd_type);
             console.log("buffer_start:", buffer_start);
             console.log("buffer_ptr:", bufferPtr);
             console.log("buffer_end:", buffer_end);
             console.log("buffer_len:", buffer_len);
-            console.log("hex buffer_start", bytesToHex(buffer_start.readByteArray(buffer_len)));
+            console.log("hex buffer_start", buffer_hex);
+
             console.log('----------------------');
         },
         onLeave(retval) {
